82765a33e5df8903b924a04a08987a05fe034502
DESKTOP-LK7G75J\whgpw <whgpwn1776@gmail.com>
2022-11-28 17:32:36 +0900

    first commit

---
+++ src/main/java/com/planE/back_source/BackSourceApplication.java
@@ -0,0 +1,13 @@
+package com.planE.back_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planE/back_source/BackSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planE.back_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
