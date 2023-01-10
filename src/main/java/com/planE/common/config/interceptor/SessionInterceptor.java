c4fc98ba9228cae42c1d158065401893f87c3027
dofany <kimdohwan17@gmail.com>
2023-01-10 18:15:48 +0900

    김도환 | 인터셉터 postman 테스트 예외처리

--- src/main/java/com/planE/common/config/interceptor/SessionInterceptor.java
+++ src/main/java/com/planE/common/config/interceptor/SessionInterceptor.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
+
 @Slf4j
 public class SessionInterceptor implements HandlerInterceptor {
     @Override
@@ -15,14 +17,22 @@ public class SessionInterceptor implements HandlerInterceptor {
         log.info("============================= 인증 체크 인터셉터 실행 : {} =============================", requestURI);
         HttpSession session = request.getSession(false);
 
-        //인증이 불가능한 사용자의 접근 검증
-        if (session == null || session.getAttribute(SessionConst.SESSION_USER_EMAIL) == null) {
-            log.info("============================= 미인증 사용자 요청 =============================");
-            response.setStatus(403);
 
-            //컨트롤러 호출을 막기 위해 false 반환
-            return false;
 
+        // 포스트맨 테스트 예외처리
+        if("postman-test".equals(request.getHeader("Postman"))) {
+            return true;
+
+        } else {
+            //인증이 불가능한 사용자의 접근 검증
+            if (session == null || session.getAttribute(SessionConst.SESSION_USER_EMAIL) == null) {
+                log.info("============================= 미인증 사용자 요청 =============================");
+                response.setStatus(403);
+
+                //컨트롤러 호출을 막기 위해 false 반환
+                return false;
+
+            }
         }
 
         //인증이 가능한 사용자는 다음 컨트롤러 호출
